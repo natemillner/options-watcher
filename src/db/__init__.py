910e6818cc73568e2f408437acc2b1e27c55a7f2
Nate Millner <nmillner@aegis-hedging.com>
2025-05-20 10:22:12 -0500

    create tables on start up.

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,4 +1,5 @@
 from .inmemory import create_db, get_db
 from .pgdb import init_db_pool, insert_trade
+from .tables import Base
 
-__all__ = ["get_db", "create_db", "init_db_pool", "insert_trade"]
+__all__ = ["get_db", "create_db", "init_db_pool", "insert_trade", "Base"]
