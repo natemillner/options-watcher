d02449c7bb9738a0ba32d9f75a5636a2936d5f6f
Nate Millner <nmillner@aegis-hedging.com>
2025-05-28 08:34:32 -0500

    fix maybe?

--- main.py
+++ main.py
@@ -19,23 +19,27 @@ def main():
     nyse = mcal.get_calendar("NYSE")
     
     while True:
-        now_ts = pd.Timestamp.now(tz="US/Eastern")
-        today = now_ts.date()
+        now_et = pd.Timestamp.now(tz="US/Eastern")
+        today = now_et.date().strftime("%Y-%m-%d")
 
-        sched = nyse.schedule(
-            start_date=today,
-            end_date=today
-        ).tz_convert("US/Eastern")
+        sched = nyse.schedule(start_date=today, end_date=today)
 
-        is_open = False
-        if not sched.empty:
-            is_open = nyse.open_at_time(sched, now_ts)
+        if sched.empty:
+            logging.info("Market closed today. Retrying in  5 min…")
+            time.sleep(300)
+            continue
 
-        if is_open:
-            logger.info("Market is open, starting WebSocket connection.")
+        sched = sched.tz_localize("UTC")
+        sched = sched.tz_convert("US/Eastern")
+
+        mo_et = sched.iloc[0]["market_open"]
+        mc_et = sched.iloc[0]["market_close"]
+
+        if mo_et <= now_et <= mc_et:
+            logging.info("Market is open. Starting WebSocket…")
             break
         else:
-            logger.info("Market is closed, retrying in 5 minutes…")
+            logging.info("Outside market hours. Retrying in 5 min…")
             time.sleep(300)
 
     asyncio.run(ws_connect())
